d0e9e981abda400be61731ea155d426eef586ceb
Jason Rogena <jason@rogena.me>
2019-06-09 17:24:50 +0300

    Get AWS instances from all regions
    Fetch the list of all regions, then instances from those regions,
    instead of just the default AWS region.
    
    Fixes #1
    
    Signed-off-by: Jason Rogena <jason@rogena.me>

--- billing/aws.go
+++ billing/aws.go
@@ -1,13 +1,57 @@
 package billing
 
 import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func getAwsVirtualMachines() ([]*VirtualMachine, error) {
+	virtualMachines := []*VirtualMachine{}
+	regions, regionErr := getAwsRegions()
+	if regionErr != nil {
+		return nil, regionErr
+	}
+
+	for _, curRegion := range regions {
+		curVirtMachines, curRegErr := getAwsVirtualMachinesInRegion(&curRegion)
+		if curRegErr != nil {
+			return nil, curRegErr
+		}
+
+		virtualMachines = append(virtualMachines, curVirtMachines...)
+	}
+
+	return virtualMachines, nil
+}
+
+func getAwsRegions() ([]string, error) {
+	regions := []string{}
+	session := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	ec2Service := ec2.New(session)
+	awsRegions, regionErr := ec2Service.DescribeRegions(nil)
+	for _, curRegion := range awsRegions.Regions {
+		regions = append(regions, *curRegion.RegionName)
+	}
+	if regionErr != nil {
+		return nil, regionErr
+	}
+
+	return regions, nil
+}
+
+func getAwsVirtualMachinesInRegion(region *string) ([]*VirtualMachine, error) {
+	fmt.Printf("Getting AWS EC2 instances in %s\n", *region)
+	awsConfig := aws.Config{
+		Region: region}
 	// Load session from shared config
 	session := session.Must(session.NewSessionWithOptions(session.Options{
+		Config:            awsConfig,
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
