b8d5b2022d3dee128c356c568404fa0a5284349e
Jason Rogena <jason@rogena.me>
2019-09-19 16:43:42 +0300

    Add tests for GetNewResourceIndex
    Add tests for the following scenarios GetNewResourceIndex should be able
    to handle:
    - two resources with duplicate indexes provided
    - two resources with duplicate indexes provided and index greater than 0
    - error thrown when provided resource ID is not found
    - resources that don't need to be assigned new indexes
    
    Signed-off-by: Jason Rogena <jason@rogena.me>

---
+++ infra/index/calculate/calculate_test.go
@@ -0,0 +1,137 @@
+package calculate
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/onaio/sre-tooling/libs/cloud"
+)
+
+// Test new index when two resources with duplicate indexes provided
+func TestGetNewResourceIndexDuplicateIndex(t *testing.T) {
+	// Test new index when two resources with duplicate indexes provided
+	resource1Id := "resource1"
+	indexTag := "indexTag"
+	index := "0"
+	resource1 := cloud.Resource{
+		Provider:   "testProvider",
+		ID:         resource1Id,
+		Location:   "eu-central-1a",
+		LaunchTime: time.Now(),
+		Tags:       map[string]string{indexTag: index},
+		Properties: map[string]string{}}
+	resource2 := cloud.Resource{
+		Provider:   "testProvider",
+		ID:         "resource2",
+		Location:   "eu-central-1a",
+		LaunchTime: time.Now(),
+		Tags:       map[string]string{indexTag: index},
+		Properties: map[string]string{}}
+	calculatedIndex, err := GetNewResourceIndex(
+		&resource1Id,
+		&indexTag,
+		[]*cloud.Resource{&resource1, &resource2})
+	if calculatedIndex != 1 {
+		t.Errorf("Index for resource1 = %d; want 1", calculatedIndex)
+	}
+	if err != nil {
+		t.Errorf("Error for resource1 = '%s'; want nil", err.Error())
+	}
+}
+
+// Test new index when two resources with duplicate indexes provided
+// and index greater than 0
+func TestGetNewResourceIndexDuplicateIndexGreaterZero(t *testing.T) {
+	resource1Id := "resource1"
+	indexTag := "indexTag"
+	index := "2"
+	resource1 := cloud.Resource{
+		Provider:   "testProvider",
+		ID:         resource1Id,
+		Location:   "eu-central-1a",
+		LaunchTime: time.Now(),
+		Tags:       map[string]string{indexTag: index},
+		Properties: map[string]string{}}
+	resource2 := cloud.Resource{
+		Provider:   "testProvider",
+		ID:         "resource2",
+		Location:   "eu-central-1a",
+		LaunchTime: time.Now(),
+		Tags:       map[string]string{indexTag: index},
+		Properties: map[string]string{}}
+	calculatedIndex, err := GetNewResourceIndex(
+		&resource1Id,
+		&indexTag,
+		[]*cloud.Resource{&resource1, &resource2})
+	if calculatedIndex != 0 {
+		t.Errorf("Index for resource1 = %d; want 1", calculatedIndex)
+	}
+	if err != nil {
+		t.Errorf("Error for resource1 = '%s'; want nil", err.Error())
+	}
+}
+
+// Test if error thrown when provided resource ID is not found
+func TestGetNewResourceIndexResourceNotFound(t *testing.T) {
+	resource1Id := "resource1"
+	indexTag := "indexTag"
+	resource1 := cloud.Resource{
+		Provider:   "testProvider",
+		ID:         resource1Id,
+		Location:   "eu-central-1a",
+		LaunchTime: time.Now(),
+		Tags:       map[string]string{indexTag: "0"},
+		Properties: map[string]string{}}
+	_, err := GetNewResourceIndex(
+		&resource1Id,
+		&indexTag,
+		[]*cloud.Resource{&resource1})
+	if err == nil {
+		t.Errorf("Error for resource1 = nil; want != nil")
+	}
+}
+
+// Test resources that don't need to be assigned new indexes (because their
+// current index is unique)
+func TestGetNewResourceIndexIndexOk(t *testing.T) {
+	resource1Id := "resource1"
+	indexTag := "indexTag"
+	resource1Index := 0
+	resource1 := cloud.Resource{
+		Provider:   "testProvider",
+		ID:         resource1Id,
+		Location:   "eu-central-1a",
+		LaunchTime: time.Now(),
+		Tags:       map[string]string{indexTag: strconv.Itoa(resource1Index)},
+		Properties: map[string]string{}}
+	resource2Index := 1
+	resource2Id := "resource2"
+	resource2 := cloud.Resource{
+		Provider:   "testProvider",
+		ID:         resource2Id,
+		Location:   "eu-central-1a",
+		LaunchTime: time.Now(),
+		Tags:       map[string]string{indexTag: strconv.Itoa(resource2Index)},
+		Properties: map[string]string{}}
+	calculatedIndex, err := GetNewResourceIndex(
+		&resource1Id,
+		&indexTag,
+		[]*cloud.Resource{&resource1, &resource2})
+	if calculatedIndex != resource1Index {
+		t.Errorf("Index for resource1 = %d; want %d", calculatedIndex, resource1Index)
+	}
+	if err == nil {
+		t.Errorf("Error for resource1 = nil; want != nil")
+	}
+	calculatedIndex, err = GetNewResourceIndex(
+		&resource2Id,
+		&indexTag,
+		[]*cloud.Resource{&resource1, &resource2})
+	if calculatedIndex != resource2Index {
+		t.Errorf("Index for resource2 = %d; want %d", calculatedIndex, resource2Index)
+	}
+	if err == nil {
+		t.Errorf("Error for resource2 = nil; want != nil")
+	}
+}
