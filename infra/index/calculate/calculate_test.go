3f6482fb555379d5ab08edcbccfbf656711c6f50
Jason Rogena <jason@rogena.me>
2019-09-19 17:16:20 +0300

    Add tests for getResourceIndex()
    Add tests for getResourceIndex() with the following scenarios:
    - if index tag is an empty string
    - if index tag not set
    - if index tag value is not a number
    - if index tag has a number
    
    Signed-off-by: Jason Rogena <jason@rogena.me>

--- infra/index/calculate/calculate_test.go
+++ infra/index/calculate/calculate_test.go
@@ -135,3 +135,58 @@ func TestGetNewResourceIndexIndexOk(t *testing.T) {
 		t.Errorf("Error for resource2 = nil; want != nil")
 	}
 }
+
+// Test if 0 gotten if index tag is an empty string
+func TestGetResourceIndexEmptyString(t *testing.T) {
+	indexTag := "indexTag"
+	resource1 := cloud.Resource{
+		Tags: map[string]string{indexTag: ""}}
+	index, err := getResourceIndex(&resource1, &indexTag)
+	if err != nil {
+		t.Errorf("Error for getResourceIndex() = '%s'; want nil", err.Error())
+	}
+	if index != 0 {
+		t.Errorf("Index = '%d'; want '%d'", index, 0)
+	}
+}
+
+// Test if 0 gotten if index tag not set
+func TestGetResourceIndexTagNotSet(t *testing.T) {
+	indexTag := "indexTag"
+	resource1 := cloud.Resource{
+		Tags: map[string]string{}}
+	index, err := getResourceIndex(&resource1, &indexTag)
+	if err != nil {
+		t.Errorf("Error for getResourceIndex() = '%s'; want nil", err.Error())
+	}
+	if index != 0 {
+		t.Errorf("Index = '%d'; want '%d'", index, 0)
+	}
+}
+
+// Test if error gotten if index tag value is not a number
+func TestGetResourceIndexNonNumberIndex(t *testing.T) {
+	indexTag := "indexTag"
+	resource1 := cloud.Resource{
+		Tags: map[string]string{indexTag: "dfd"}}
+	_, err := getResourceIndex(&resource1, &indexTag)
+	if err == nil {
+		t.Errorf("Error for getResourceIndex() = nil; want != nil")
+	}
+}
+
+// Test if right number gotten if index tag has a number
+func TestGetResourceIndexOk(t *testing.T) {
+	indexTag := "indexTag"
+	indexString := "231232"
+	resource1 := cloud.Resource{
+		Tags: map[string]string{indexTag: indexString}}
+	index, err := getResourceIndex(&resource1, &indexTag)
+	if err != nil {
+		t.Errorf("Error for getResourceIndex() = %s; want nil", err.Error())
+	}
+	indexStrVal, _ := strconv.Atoi(indexString)
+	if indexStrVal != index {
+		t.Errorf("Index = %d; want %d", index, indexStrVal)
+	}
+}
