2ab84ef169e39c181d2bfd718fd2f873168239e2
Jason Rogena <jason@rogena.me>
2020-04-18 18:39:04 +0300

    Move cloud resource table code into lib
    Move the code responsible for rendering cloud resources in a table into
    the cloud library, from the query sub-command, in anticipation that this
    will be reused elsewhere.
    
    Signed-off-by: Jason Rogena <jason@rogena.me>

--- infra/query/query.go
+++ infra/query/query.go
@@ -1,12 +1,8 @@
 package query
 
 import (
-	"bytes"
 	"flag"
-	"sort"
-	"strings"
 
-	"github.com/olekukonko/tablewriter"
 	"github.com/onaio/sre-tooling/libs/cli"
 	"github.com/onaio/sre-tooling/libs/cli/flags"
 	"github.com/onaio/sre-tooling/libs/cloud"
@@ -24,7 +20,7 @@ type Query struct {
 	tagFlag               *flags.StringArray
 	showFlag              *flags.StringArray
 	hideHeadersFlag       *bool
-	csv                   *bool
+	csvFlag               *bool
 	fieldSeparatorFlag    *string
 	resourceSeparatorFlag *string
 	listFieldsFlag        *bool
@@ -36,14 +32,14 @@ func (query *Query) Init(helpFlagName string, helpFlagDescription string) {
 	query.flagSet = flag.NewFlagSet(query.GetName(), flag.ExitOnError)
 	query.helpFlag = query.flagSet.Bool(helpFlagName, false, helpFlagDescription)
 	query.providerFlag, query.regionFlag, query.typeFlag, query.tagFlag = cloud.AddFilterFlags(query.flagSet)
-	query.showFlag = new(flags.StringArray)
-	query.flagSet.Var(query.showFlag, "show", "Resource's field to be shown e.g tag:Name. Multiple values can be provided by specifying multiple -show")
-	query.hideHeadersFlag = query.flagSet.Bool("hide-headers", false, "Whether to hide the names of the fields shown")
-	query.csv = query.flagSet.Bool("csv", false, "Whether output should follow the CSV standard")
-	query.fieldSeparatorFlag = query.flagSet.String("field-separator", "\t", "What to use to separate displayed fields. Only applies if -csv is enabled")
-	query.resourceSeparatorFlag = query.flagSet.String("resource-separator", "\n", "What to use to separate displayed resources. Only applies of -list-fields or -csv is enabled")
-	query.listFieldsFlag = query.flagSet.Bool("list-fields", false, "Whether to just list the fields available to be displayed, instead of the resources")
-	query.defaultFieldValueFlag = query.flagSet.String("default-field-value", "", "Text that should be set if a resource's queried field doesn't have a value")
+	query.showFlag,
+		query.hideHeadersFlag,
+		query.csvFlag,
+		query.fieldSeparatorFlag,
+		query.resourceSeparatorFlag,
+		query.listFieldsFlag,
+		query.defaultFieldValueFlag = cloud.AddResourceTableFlags(query.flagSet)
+
 	query.subCommands = []cli.Command{}
 }
 
@@ -79,87 +75,20 @@ func (query *Query) Process() {
 		cli.ExitCommandExecutionError()
 	}
 
-	rows := make([]map[string]string, len(allResources))
-	headers := make(map[string]bool)
-	for rowIndex, curResource := range allResources {
-		headers, rows = query.addFields(headers, rows, rowIndex, curResource.Tags, "tag")
-		headers, rows = query.addFields(headers, rows, rowIndex, curResource.Properties, "property")
-	}
-
-	output := ""
-	displayedHeaders := []string{}
-	for curHeader, includeHeader := range headers {
-		if includeHeader {
-			displayedHeaders = append(displayedHeaders, curHeader)
-		}
-	}
-	sort.Strings(displayedHeaders)
-	if *query.listFieldsFlag {
-		output = strings.Join(displayedHeaders, *query.resourceSeparatorFlag)
-	} else {
-		buf := new(bytes.Buffer)
-		table := tablewriter.NewWriter(buf)
-		table.SetAlignment(tablewriter.ALIGN_LEFT)
-		table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-		table.SetAutoFormatHeaders(false)
-		if *query.csv {
-			table.SetTablePadding("")
-			table.SetAutoWrapText(false)
-			table.SetBorder(false)
-			table.SetHeaderLine(false)
-			table.SetCenterSeparator("")
-			table.SetRowLine(false)
-			table.SetNewLine(*query.resourceSeparatorFlag)
-			table.SetColumnSeparator(*query.fieldSeparatorFlag)
-		}
-
-		if !*query.hideHeadersFlag {
-			table.SetHeader(displayedHeaders)
-		}
-
-		for _, curRow := range rows {
-			rowSlice := []string{}
-			for _, curHeader := range displayedHeaders {
-				fieldVal := *query.defaultFieldValueFlag
-				if curField, ok := curRow[curHeader]; ok {
-					fieldVal = curField
-				}
-
-				rowSlice = append(rowSlice, fieldVal)
-			}
-			table.Append(rowSlice)
-		}
-
-		table.Render()
-		output = buf.String()
+	rt := new(cloud.ResourceTable)
+	rt.Init(
+		query.showFlag,
+		query.hideHeadersFlag,
+		query.csvFlag,
+		query.fieldSeparatorFlag,
+		query.resourceSeparatorFlag,
+		query.listFieldsFlag,
+		query.defaultFieldValueFlag)
+	output, outputErr := rt.Render(allResources)
+	if outputErr != nil {
+		notification.SendMessage(outputErr.Error())
+		cli.ExitCommandExecutionError()
 	}
 
 	notification.SendMessage(output)
-}
-
-func (query *Query) addFields(headers map[string]bool, rows []map[string]string, rowIndex int, newFields map[string]string, fieldType string) (map[string]bool, []map[string]string) {
-	if rows[rowIndex] == nil {
-		rows[rowIndex] = make(map[string]string)
-	}
-	for curKey, curVal := range newFields {
-		fieldName := fieldType + ":" + curKey
-		rows[rowIndex][fieldName] = curVal
-		headers[fieldName] = query.considerField(fieldName)
-	}
-
-	return headers, rows
-}
-
-func (query *Query) considerField(field string) bool {
-	if len(*query.showFlag) == 0 {
-		return true
-	}
-
-	for _, curField := range *query.showFlag {
-		if curField == field {
-			return true
-		}
-	}
-
-	return false
 }
