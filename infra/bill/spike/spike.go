d5b57b14775285dd422df8438adc7b99973574df
Evans Murithi <murithievans80@gmail.com>
2020-09-02 16:37:04 +0300

    fixup! Add sub command to calculate cost uage spikes

--- infra/bill/spike/spike.go
+++ infra/bill/spike/spike.go
@@ -2,8 +2,9 @@ package spike
 
 import (
 	"flag"
+	"fmt"
 
-	"github.com/onaio/sre-tooling/libs/cloud"
+	"github.com/onaio/sre-tooling/libs/infra"
 
 	"github.com/onaio/sre-tooling/libs/cli"
 
@@ -11,35 +12,67 @@ import (
 )
 
 const name string = "spike"
+const outputFormatPlain = "plain"
+const outputFormatMarkdown = "markdown"
 
 type Spike struct {
 	helpFlag              *bool
 	flagSet               *flag.FlagSet
-	providerFlag          *flags.StringArray
-	regionFlag            *flags.StringArray
-	typeFlag              *flags.StringArray
-	tagFlag               *flags.StringArray
+	granularityFlag       *string
+	startDateFlag         *string
+	endDateFlag           *string
 	showFlag              *flags.StringArray
+	providerFlag          *flags.StringArray
 	hideHeadersFlag       *bool
 	csvFlag               *bool
 	fieldSeparatorFlag    *string
 	resourceSeparatorFlag *string
 	listFieldsFlag        *bool
 	defaultFieldValueFlag *string
+	outputFormatFlag      *string
 	subCommands           []cli.Command
 }
 
 func (spike *Spike) Init(helpFlagName string, helpFlagDescription string) {
 	spike.flagSet = flag.NewFlagSet(spike.GetName(), flag.ExitOnError)
 	spike.helpFlag = spike.flagSet.Bool(helpFlagName, false, helpFlagDescription)
-	spike.providerFlag, spike.regionFlag, spike.typeFlag, spike.tagFlag = cloud.AddFilterFlags(spike.flagSet)
+	providerFlag := new(flags.StringArray)
+	spike.flagSet.Var(
+		providerFlag,
+		"filter-provider",
+		"Name of provider to filter using. Multiple values can be provided by specifying multiple -filter-provider",
+	)
+	spike.providerFlag = providerFlag
+	spike.granularityFlag = spike.flagSet.String(
+		"granularity",
+		"DAILY",
+		"Cost granularity to use. Can be MONTHLY, DAILY or HOURLY.",
+	)
+	spike.startDateFlag = spike.flagSet.String(
+		"start-date",
+		"",
+		"Start date for retrieving costs. Start date is inclusive. Should be in the format YYYY-MM-DD.",
+	)
+	spike.endDateFlag = spike.flagSet.String(
+		"end-date",
+		"",
+		"End date for retrieving costs. End date is exclusive. Should be in the format YYYY-MM-DD.",
+	)
+	spike.outputFormatFlag = spike.flagSet.String(
+		"output-format",
+		outputFormatPlain,
+		fmt.Sprintf(
+			"How to format the full output text. Possible values are '%s' and '%s'.",
+			outputFormatPlain,
+			outputFormatMarkdown))
+
 	spike.showFlag,
 		spike.hideHeadersFlag,
 		spike.csvFlag,
 		spike.fieldSeparatorFlag,
 		spike.resourceSeparatorFlag,
 		spike.listFieldsFlag,
-		spike.defaultFieldValueFlag = cloud.AddResourceTableFlags(spike.flagSet)
+		spike.defaultFieldValueFlag = infra.AddResourceTableFlags(spike.flagSet)
 
 	spike.subCommands = []cli.Command{}
 }
